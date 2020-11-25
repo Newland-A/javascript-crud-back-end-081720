eee8f06e35813dbe83aac81b54534b1c52262946
cjbrock <corinna@flatironschool.com>
2020-11-25 15:40:24 -0500

    post js crud like/update group code

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_24_161422) do
+ActiveRecord::Schema.define(version: 2020_11_25_160826) do
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "content"
-    t.integer "likes"
+    t.integer "likes", default: 0
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
